a252cdcf876225ec24fa05b7ad9924847f87c2df
Mugunthan <87799495+Micoder-dev@users.noreply.github.com>
2022-06-01 14:57:52 +0530

    bottom nav ui updated

--- app/src/main/java/com/micoder/bottomnavigationui2/MainActivity.java
+++ app/src/main/java/com/micoder/bottomnavigationui2/MainActivity.java
@@ -1,14 +1,49 @@
 package com.micoder.bottomnavigationui2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
+
 public class MainActivity extends AppCompatActivity {
+
+    ChipNavigationBar chipNavigationBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        chipNavigationBar = findViewById(R.id.bottom_nav_menu);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new DashboardFragment()).commit();
+
+        bottomMenu();
+    }
+
+    private void bottomMenu() {
+
+        chipNavigationBar.setOnItemSelectedListener(new ChipNavigationBar.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(int i) {
+                Fragment fragment = null;
+                switch (i) {
+                    case R.id.bottom_nav_dashboard:
+                        fragment = new DashboardFragment();
+                        break;
+                    case R.id.bottom_nav_manage:
+                        fragment = new ManageFragment();
+                        break;
+                    case R.id.bottom_nav_profile:
+                        fragment = new ProfileFragment();
+                        break;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
+            }
+        });
+
     }
 }
